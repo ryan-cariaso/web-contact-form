49a6dd8333f1522f51059eec2b1883dae452d0b1
ryan-cariaso <ryanchristophercariaso@gmail.com>
2021-07-13 10:44:10 -0700

    new change commit

--- form.js
+++ form.js
@@ -1,27 +1,15 @@
-var md = new Remarkable({
-  html: true,
-  xhtmlOut: false,
-  breaks: false,
-  linkify: true,
-  typographer: false,
-  quotes: "“”‘’"
-});
+function results() {
+  var fname = document.getElementById('fname').value
+  var lname = document.getElementById('lname').value
+  var email = document.getElementById('email').value
+  var reason = document.getElementById('reason').value
+  var comment = document.getElementById('comment').value
 
-function sumbit(){
-  var name = document.getElementById("fname").value + " " + document.getElementById("mi").value + ". " + document.getElementById("lname").value;
-  var email = document.getElementById("email").value;
-  var reason = $("input[name=reason]:checked", "#reason").val();
-  var notparsedcomment = document.getElementById("comment").value;
-  var text = `Name: ${name}<br>Email: ${email}<br>Contact Reason: ${reason}<br>Comment: ${comment}`;
-  document.getElementById("form").style.display = "none";
-  document.getElementById("result").style.display = "block";
-  document.getElementById("result").innerHTML = text;
+  document.write("<h1>Thank you!</h1>")
+  document.write("<h2>Review information below: </h2>")
+  document.write("Name: " + fname + " " + lname + "<br>")
+  document.write("Email: " + email + "<br>")
+  document.write("Reason for contact: " + reason + "<br>")
+  document.write("Comment: " + comment + "<br>")
 }
-$("textarea").keyup(function() {
-  if (this.value.length > $(this).attr("maxlength")) {
-    return false
-  }
-  $(this)
-    .next()
-    .html(this.value.length + "/" + $(this).attr("maxlength"));
-});
+
